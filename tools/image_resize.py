7dd9d02d9efe2e1fbdfc1605eebdfe437b7d3954
minari-c <76611495+minari-c@users.noreply.github.com>
2022-12-29 16:07:20 +0900

    change image file

--- tools/image_resize.py
+++ tools/image_resize.py
@@ -1,12 +1,12 @@
 from time import sleep
 import cv2
 
-img = cv2.imread('../resource/uni.jpg')
-cv2.imwrite('../resize_img/uni.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 100])
+img = cv2.imread('../resource/cat.jpg')
+cv2.imwrite('../resize_img/cat.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 100])
 
-img2 = cv2.imread('../resize_img/uni.png')
-img2 = cv2.resize(img2, (3840 * 4, 2160 * 4), interpolation=cv2.INTER_LANCZOS4)
-cv2.imwrite('../resize_img/uni.png', img2)
+img2 = cv2.imread('../resize_img/cat.png')
+img2 = cv2.resize(img2, (3840, 2160), interpolation=cv2.INTER_LANCZOS4)
+cv2.imwrite('../resize_img/cat.png', img2)
 
 print(img2.shape)
 cv2.imshow('Color', img2)
